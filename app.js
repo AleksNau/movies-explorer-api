2625e4044cd4205d97f1c69e0c0b87e2b10ae908
AleksNau <workciti@yandex.ru>
2023-09-14 10:27:18 +0300

    роуты авторизации в отдельном файле

--- app.js
+++ app.js
@@ -6,15 +6,10 @@ const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 require('dotenv').config();
 
-const { validationCreateUser, validationLogin } = require('./middlewares/validation');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const auth = require('./middlewares/auth');
+const loggedInCheck = require('./middlewares/auth');
 const handleError = require('./middlewares/handleError');
-const {
-  createProfile,
-  login,
-} = require('./controllers/users');
-
+const authoriazation = require('./routes/sign')
 const { PORT = 3000, MONGODB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 mongoose.connect(MONGODB_URL, {
@@ -43,10 +38,9 @@ const router = require('./routes/index');
 
 app.use(express.json());
 
-app.post('/signin', validationLogin, login);
-app.post('/signup', validationCreateUser, createProfile);
+app.use(authoriazation);
 
-app.use(auth);
+app.use(loggedInCheck);
 app.use(router);
 
 app.use(errorLogger);
---
+++ routes/sign.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const {
+ login, createProfile,
+} = require('../controllers/users');
+
+const {
+  validationLogin, validationCreateUser,
+} = require('../middlewares/validation');
+// создание пользователя
+router.post('/signin', validationLogin, login);
+router.post('/signup', validationCreateUser, createProfile);
+
+module.exports = router;
